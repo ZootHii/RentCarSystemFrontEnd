dce0df9230b254359e612f661f86ad3242bff14d
Ahmet <ahmet-yildirim_5858@outlook.com>
2021-03-25 18:25:41 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'rent-car-system-frontend';
+}
+
+// 1. npm install -g @angular/cli
+// 2. C:\Users\ahmet\Desktop\WebstormProjects\angular-workspace\>ng new rent-car-system-frontend
+// 3. 
